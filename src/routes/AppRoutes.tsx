906be166fa46b3829110912876a6b9cd9614e203
Varsha <varshavsl.30@gmail.com>
2026-01-16 14:52:48 +0530

    added protected route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login/Login";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
+            <Route element={<ProtectedRoute />}>
+                <Route path="/home" element={<Home />} />
+                {/* <Route path="*" element={<Navigate to="/login" />} /> */}
+            </Route>
+        </Routes >
     );
 };
 
