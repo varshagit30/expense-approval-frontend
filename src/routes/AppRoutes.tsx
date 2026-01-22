759a2fdc590a7a2adf74b788d2097b31072a225b
Varsha <varshavsl.30@gmail.com>
2026-01-22 14:05:02 +0530

    sidebar layout

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,16 +2,33 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Home from "../pages/Home/Home";
 import ProtectedRoute from "./ProtectedRoute";
+import ExpenseList from "../pages/Expenses/ExpenseList";
+import AppLayout from "../layouts/AppLayout";
 
 const AppRoutes = () => {
     return (
+        // <Routes>
+        //     <Route path="/login" element={<Login />} />
+        //     <Route path="/home" element={<Home />} />
+        //     <Route path="/expenses" element={<ExpenseList />} />
+        //     <Route element={<ProtectedRoute />}>
+        //         {/* <Route path="*" element={<Navigate to="/login" />} /> */}
+        //     </Route>
+        // </Routes >
+
         <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
+
+            <Route element={<AppLayout />} >
+                <Route path="/home" element={<Home />} />
+                <Route path="/expenses" element={<ExpenseList />} /></Route>
             <Route element={<ProtectedRoute />}>
-                {/* <Route path="*" element={<Navigate to="/login" />} /> */}
             </Route>
-        </Routes >
+
+            <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+
     );
 };
 
