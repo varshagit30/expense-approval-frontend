6b4186d73e688cf270bb34c50fc024a54ac46e10
Varsha <varshavsl.30@gmail.com>
2026-01-26 20:24:09 +0530

    added create expense button

--- src/pages/Expenses/ExpenseList.tsx
+++ src/pages/Expenses/ExpenseList.tsx
@@ -1,6 +1,9 @@
-import { Table, Tag, Typography, message } from "antd";
+import { Button, Table, Tag, Typography, message } from "antd";
 import { useEffect, useState } from "react";
 import { getExpenses, type Expense } from "../../api/expenses";
+import { PlusOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import "../../pages/Home/home.css";
 
 const { Title } = Typography;
 
@@ -16,6 +19,7 @@ const statusColorMap: Record<string, string> = {
 const ExpenseList = () => {
     const [expenses, setExpenses] = useState<Expense[]>([]);
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetchExpenses();
@@ -38,6 +42,8 @@ const ExpenseList = () => {
             title: "Title",
             dataIndex: "title",
             key: "title",
+            sorter: (a: Expense, b: Expense) =>
+                a.title.localeCompare(b.title),
         },
         {
             title: "Description",
@@ -48,6 +54,12 @@ const ExpenseList = () => {
             title: "Status",
             dataIndex: "currentStatus",
             key: "currentStatus",
+            filters: Object.keys(statusColorMap).map((status) => ({
+                text: status.replaceAll("_", " "),
+                value: status,
+            })),
+            onFilter: (value: any, record: Expense) =>
+                record.currentStatus === value,
             render: (status: string) => (
                 <Tag color={statusColorMap[status] || "default"}>
                     {status.replaceAll("_", " ")}
@@ -62,9 +74,23 @@ const ExpenseList = () => {
     ];
 
     return (
-        <>
-            <Title level={3}>Expenses</Title>
+        <div className="page-container">
+            {/* Header Row */}
+            <div className="page-header">
+                <Title level={3} style={{ margin: 0 }}>
+                    Expenses
+                </Title>
+                <Button
+                    type="primary"
+                    icon={<PlusOutlined />}
+                    size="large"
+                    onClick={() => navigate("/expenses/new")}
+                >
+                    Create New Expense
+                </Button>
+            </div>
 
+            {/* Table */}
             <Table
                 rowKey="id"
                 columns={columns}
@@ -72,7 +98,7 @@ const ExpenseList = () => {
                 loading={loading}
                 pagination={{ pageSize: 5 }}
             />
-        </>
+        </div>
     );
 };
 
